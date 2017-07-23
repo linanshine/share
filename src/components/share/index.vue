ee7870373735cce69296b8b49cd3a90018401c7e
unknown <lnnlshine@gmail.com>
2017-07-23 16:03:52 +0800

    ‘首页样式修改

--- src/components/share/index.vue
+++ src/components/share/index.vue
@@ -1,15 +1,17 @@
 <template>
  <v-container fluid class="text-xs-center">
+    <div class="hr"> </div>
     <v-layout row wrap>
     
       <v-flex xs12 >
 
         <v-card height="185px" class="elevation-0 white">
 
-
           <v-card-text>
             <div v-for='item in indexArr'>
-              <v-btn flat :to='item.route'>{{item.text}}</v-btn>
+            <router-link class="routerLink"  :to='item.route'>
+              {{item.text}}</v-btn>
+            </router-link>
             </div>
           </v-card-text>
         </v-card>
@@ -28,3 +30,22 @@
     }
 	}
 </script>
+<style scoped>
+  .hr {
+    border-bottom: 1px solid #dddddd;
+    margin: 1rem 0;
+}
+
+.routerLink {
+  display: block;
+    width: 100%;
+    height: 3rem;
+    line-height: 3rem;
+    color: #999999;
+    border: 1px solid #dddddd;
+    text-decoration: none;
+    font-size: 0.8rem;
+    margin: 0.3rem auto 0.7rem;
+    border-radius: 5px;
+}
+</style>
